473a7f3de4ece12a64952a06a8e70727ae895af8
Carlos Camacho <carlosdcamachoed@gmail.com>
2026-02-11 14:20:46 -0500

    added prod

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,37 @@
+import { dev } from "$app/environment";
+
 // place files you want to import through the `$lib` alias in this folder.
-export const BACKEND_DOMAIN = "http://localhost:3000";
+export const BACKEND_DOMAIN = dev
+  ? "http://localhost:3000"
+  : "https://api.tutoring.frqubit.net";
+export const COGNITO_BASE_URI =
+  "https://us-east-17bvoomwr4.auth.us-east-1.amazoncognito.com";
+
+function getSigninUrl(): string {
+  const out = new URL("/login/continue", COGNITO_BASE_URI);
+  out.search = new URLSearchParams({
+    client_id: "2cq5ns7arvn12i9ll973hdj0f9",
+    response_type: "code",
+    scope: "email+openid+phone",
+    redirect_uri: new URL("/auth", BACKEND_DOMAIN).toString(),
+  }).toString();
+
+  // URLSearchParams is stupid and replaces the pluses in the
+  // scope with %2B :/
+  return out.toString().replaceAll("%2B", "+");
+}
+
+function getSignoutUrl(): string {
+  const out = new URL("/logout", COGNITO_BASE_URI);
+  out.search = new URLSearchParams({
+    client_id: "2cq5ns7arvn12i9ll973hdj0f9",
+    logout_uri: new URL("/auth/logout", BACKEND_DOMAIN).toString(),
+    redirect_uri: new URL("/auth/logout", BACKEND_DOMAIN).toString(),
+    response_type: "code",
+  }).toString();
+
+  return out.toString();
+}
+
+export const SIGNIN_URL = getSigninUrl();
+export const SIGNOUT_URL = getSignoutUrl();
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import "./layout.css";
     import favicon from "$lib/assets/favicon.svg";
+    import { SIGNIN_URL, SIGNOUT_URL } from "$lib";
 
     let { children, data } = $props();
 </script>
@@ -11,15 +12,9 @@
         <a href="/" class="text-blue-700 text-xl font-semibold">Homepage</a>
 
         {#if !data.signed_in}
-            <a
-                href="https://us-east-17bvoomwr4.auth.us-east-1.amazoncognito.com/login/continue?client_id=2cq5ns7arvn12i9ll973hdj0f9&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth&response_type=code&scope=email+openid+phone"
-                class="text-blue-700 ml-auto">Sign in</a
-            >
+            <a href={SIGNIN_URL} class="text-blue-700 ml-auto">Sign in</a>
         {:else}
-            <a
-                href="https://us-east-17bvoomwr4.auth.us-east-1.amazoncognito.com/logout?client_id=2cq5ns7arvn12i9ll973hdj0f9&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Flogout&logout_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Flogout&response_type=code"
-                class="text-blue-700 ml-auto">Sign out</a
-            >
+            <a href={SIGNOUT_URL} class="text-blue-700 ml-auto">Sign out</a>
         {/if}
     </div>
     {@render children()}
