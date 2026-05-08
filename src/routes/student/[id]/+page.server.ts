cced9085f13f7b37cea6993cc6a0a9b57768bcee
Carlos Camacho <carlosdcamachoed@gmail.com>
2026-05-08 11:57:29 -0400

    updated student session display

--- src/routes/student/[id]/+page.server.ts
+++ src/routes/student/[id]/+page.server.ts
@@ -1,5 +1,5 @@
 import type { Actions, PageServerLoad } from "./$types";
-import { StudentFetcher } from "$lib/fetchers";
+import { SessionFetcher, StudentFetcher } from "$lib/fetchers";
 import { BACKEND_DOMAIN } from "$lib";
 import { redirect } from "@sveltejs/kit";
 
@@ -14,18 +14,29 @@ interface SessionsAndDeposits {
 }
 
 async function find_sessions_and_deposits(
-  fetcher: StudentFetcher,
+  student_fetcher: StudentFetcher,
+  session_fetcher: SessionFetcher,
   id: number,
 ): Promise<SessionsAndDeposits | null> {
-  const deposits = await fetcher.findAllDepositsOf([id]);
+  const deposits = await student_fetcher.findAllDepositsOf([id]);
 
   if (!deposits) return null;
 
-  const completed_sessions = await fetcher.findAllCompletedSessionsOf([id]);
+  const completed_sessions = await student_fetcher.findAllCompletedSessionsOf([
+    id,
+  ]);
 
   if (!completed_sessions) return null;
 
-  const future_sessions = await fetcher.findAllFutureSessionsOf([id]);
+  const future_sessions = await session_fetcher.findAllByFilter(
+    {
+      student_id: id,
+      start: undefined,
+      end: undefined,
+      completed: false,
+    },
+    [],
+  );
 
   if (!future_sessions) return null;
 
@@ -38,6 +49,7 @@ async function find_sessions_and_deposits(
 
 export const load: PageServerLoad = async ({ fetch, params, url }) => {
   const studentFetcher = new StudentFetcher(fetch, url);
+  const sessionFetcher = new SessionFetcher(fetch, url);
   const student = await studentFetcher.findOne([+params.id]);
 
   if (!student) {
@@ -46,6 +58,7 @@ export const load: PageServerLoad = async ({ fetch, params, url }) => {
 
   const sessions_and_deposits = await find_sessions_and_deposits(
     studentFetcher,
+    sessionFetcher,
     +params.id,
   );
 
