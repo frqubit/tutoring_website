07d44bf7262217b963f33055536221f9e716c751
Carlos Camacho <carlosdcamachoed@gmail.com>
2026-07-11 13:49:10 -0400

    batl: 9DC5253C14FC8BC67464FA385AFE6EE69E2B78CEE79B87DBC95C412CC73563E6

--- src/lib/components/CalendarTile.svelte
+++ src/lib/components/CalendarTile.svelte
@@ -19,9 +19,11 @@
     }
 
     const startTime = calculate_start_time();
+
+    const past_and_uncompleted = session.date.getTime() < new Date().getTime() && !session.completed
 </script>
 
-<div class="border-y px-1 flex flex-col w-full">
+<div class={`border-y px-1 flex flex-col w-full ${past_and_uncompleted && "bg-orange-300"}`}>
     <a
         class="font-bold text-blue-500"
         href={`/student/${session.students[0].id}`}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,6 +9,19 @@
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 <div class={`w-full min-h-screen p-6 ${dev && "bg-red-100"}`}>
+    {#if dev}
+        <div
+            class="fixed top-0 left-0 w-full h-full flex items-center justify-center pointer-events-none"
+        >
+            <span
+                class="
+                    text-9xl font-serif text-red-900 opacity-15
+                "
+            >
+                DEV
+            </span>
+        </div>
+    {/if}
     <div class="w-full border-b-2 mb-4 flex flex-row items-center gap-x-5">
         <a href="/" class="text-blue-700 text-xl font-semibold">Homepage</a>
         <a href="/student" class="text-blue-700 text-xl font-semibold"
